5ec397aacad4db8ee8bc04be291e4f75a852a46e
McKay Anderson <mckayanderson@mckays-mbp.lan>
2024-01-15 21:38:39 -0700

    changed filetype to .ts

---
+++ utilities/handleUserScroll.ts
@@ -0,0 +1,18 @@
+// This utility handles user scroll inputs
+// We use the 'scroll' event listener to handle inputs from all necessary devices including trackpads, mouses, and touch screens
+
+type ScrollCallback = (deltaX: number, deltaY: number) => void;
+
+const scrollListener = (onScroll: ScrollCallback): (() => void) => {
+  const handleScroll = () => {
+    onScroll(window.scrollX, window.scrollY);
+    console.log('scroll event:', window.scrollX, window.scrollY);
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  // Return a function to remove the event listener
+  return () => window.removeEventListener('scroll', handleScroll);
+};
+
+export default scrollListener;
