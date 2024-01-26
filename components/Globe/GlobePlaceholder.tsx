d794e53ce72b37aa26d2377b75c60acf7b30afea
McKay Anderson <andmckay01@gmail.com>
2024-01-25 22:38:37 -0700

    simplified

--- components/Globe/GlobePlaceholder.tsx
+++ components/Globe/GlobePlaceholder.tsx
@@ -1,21 +1,20 @@
 // This component creates the static map layer used when 3d Globe is not avialable
 
 import { FC } from 'react';
-import styled from 'styled-components';
 
-const PlaceholderImage = styled.img`
-    width: 5555px;
-    object-fit: cover;
-    height: 1500px;
-    offset: 200px;
-    position: relative;
-    top: -110px;
-    left: 0px;
-`
+const placeholderImageStyle: React.CSSProperties = {
+    width: '5555px',
+    objectFit: 'cover',
+    height: '1500px',
+    offset: '200px',  // 'offset' is not a standard CSS property and might not have the intended effect
+    position: 'relative',
+    top: '-110px',
+    left: '0px',
+};
 
 const PlaceholderMap: FC = () => {
     return (
-        <PlaceholderImage src="./map.png" alt="Map Image"/>
+        <img src="./map.png" alt="Map Image" style={placeholderImageStyle}/>
     )
 }
 
