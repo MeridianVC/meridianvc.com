9a1f4b5e9c001bcc3e2c6160a8eedb770568b32f
McKay Anderson <andmckay01@gmail.com>
2024-01-26 00:17:18 -0700

    simplified by 25%

--- app/page.tsx
+++ app/page.tsx
@@ -17,62 +17,49 @@ const Globe = dynamic(() => import('../components/Globe/Globe'), {
     loading: () => <PlaceholderMap/> // loads placeholder while 3D globe renders
 });
 
-const outerStyle: React.CSSProperties = {
-    width: '100vw',
-    maxWidth: '100%',
-    height: '100vh',
-    maxHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-    position: 'relative',
-};
-
 const globeStyle: React.CSSProperties = {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: -2
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: -2
 };
 
-const ScrollContentStyle: React.CSSProperties = {
-  backgroundColor: "transparent",
-  width: "88%",
-  height: "1500vh",  // Adjust as needed
-  minHeight: "1500vh",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  position: "absolute",
-  top: "0",  // Align to the top of the parent element
-  left: "50%",  // Center horizontally
-  transform: "translateX(-50%)",  // Translate only horizontally
+const mainContentStyle: React.CSSProperties = {
+  width: '100vw',
+  maxWidth: '100%',
+  height: '1500vh',
+  minHeight: '1500vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  position: 'absolute',
+  backgroundColor: 'transparent',
+  top: '0',
+  left: '50%',
+  transform: 'translateX(-50%)',
 };
 
 const Home: FC = () => {
-    return (
-      <>
-        <Navbar />
-        <FillVertical side="left" />
-        <FillVertical side="right" />
-        <FillHorizontal />
-        <FillHorizontal behind />
-        <div style={outerStyle}>
-            <div style={ScrollContentStyle}>
-              <Header type="H1" isBackgroundColored>Early stage venture capital for badass people. </Header>
-                <div>Hello there</div>
-              <CardWrapper>
-                  <PrinciplesCard title="Card 1" content="This is the content of Card 1." />
-                  <PrinciplesCard title="Card 2" content="This is the content of Card 2." />
-              </CardWrapper>
-            </div>
-            <Globe style={globeStyle} />
-        </div>
-      </>
-    );
+  return (
+    <>
+      <Navbar />
+      <FillVertical side='left' />
+      <FillVertical side='right' />
+      <FillHorizontal />
+      <FillHorizontal behind />
+      <main style={mainContentStyle}>
+        <Header type='H1' isBackgroundColored>Early stage venture capital for badass people. </Header>
+          <div>Hello there</div>
+        <CardWrapper>
+            <PrinciplesCard title='Card 1' content='This is the content of Card 1.' />
+            <PrinciplesCard title='Card 2' content='This is the content of Card 2.' />
+        </CardWrapper>
+      </main>
+      <Globe style={globeStyle} />
+    </>
+  );
 };
 
 export default Home;
