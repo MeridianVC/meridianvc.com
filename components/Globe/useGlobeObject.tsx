2848095ccff2bf385ff7c9463fba2acd6b274423
McKay Anderson <andmckay01@gmail.com>
2024-01-29 03:42:09 -0700

    removed old files and added newest globe object

--- components/Globe/useGlobeObject.tsx
+++ components/Globe/useGlobeObject.tsx
@@ -23,7 +23,7 @@ const useGlobeObject = ({ renderer, scene }: GlobeObjectProps) => {
 
         // Load the GLB/GLTF file
         const loader = new GLTFLoader();
-        loader.load('./world12.glb', (gltf) => {
+        loader.load('./world13.glb', (gltf) => {
 
             // Scale and position the scene
             gltf.scene.scale.set(3, 3, 3);
