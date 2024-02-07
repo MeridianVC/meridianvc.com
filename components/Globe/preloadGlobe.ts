4b0b1c4b44e96bcec3e84c15de1229c7937a60b9
McKay Anderson <andmckay01@gmail.com>
2024-02-06 22:21:00 -0700

    added new globe with drastically reduced size

--- components/Globe/preloadGlobe.ts
+++ components/Globe/preloadGlobe.ts
@@ -5,8 +5,7 @@ import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
 // Promise to load the globe and return a GLTF object
 const globePromise: Promise<GLTF> = new Promise((resolve, reject) => {
     const loader = new GLTFLoader();
-    loader.load('./globe.glb', (gltf: GLTF) => {
-        // Processing can also be done here if necessary
+    loader.load('./world-midpoly-no-normal.glb', (gltf: GLTF) => {
         resolve(gltf);
     }, undefined, (error) => reject(error));
 });
