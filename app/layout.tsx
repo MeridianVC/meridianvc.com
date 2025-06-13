aff50dfdd61606a56d73c1759dce5a16e998629b
Joey Eamigh <55670930+JoeyEamigh@users.noreply.github.com>
2025-06-12 21:55:39 -0400

    dropping vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,42 @@
-import type { Metadata } from 'next'
-import localfont from 'next/font/local'
-import { Libre_Franklin } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+import localfont from 'next/font/local';
+import { Libre_Franklin } from 'next/font/google';
+// import { Analytics } from '@vercel/analytics/react';
 import AnimationProvider from '@/components/Animation/AnimationContext';
 
 const Baskerville = localfont({
   src: [
     {
-    path: '../public/fonts/BaskervilleMTPro-Regular.otf',
-    weight: '400'
-    }
+      path: '../public/fonts/BaskervilleMTPro-Regular.otf',
+      weight: '400',
+    },
   ],
-  variable: '--font-baskerville'
-})
+  variable: '--font-baskerville',
+});
 
-const Franklin = Libre_Franklin({ 
+const Franklin = Libre_Franklin({
   subsets: ['latin'],
   weight: ['300'], //extra-lightop
   style: ['normal'],
-  variable: '--font-franklin'
-})
+  variable: '--font-franklin',
+});
 
 export const metadata: Metadata = {
   title: 'Meridian Ventures',
   description: 'Bold ideas and visionary founders',
-  metadataBase: new URL("https://meridianvc.com")
-}
+  metadataBase: new URL('https://meridianvc.com'),
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${Baskerville.variable} ${Franklin.variable}`}>
       <AnimationProvider>
         <body>
-            {children}
-            <div id="modal-root"></div>
-            <Analytics/>
+          {children}
+          <div id="modal-root"></div>
+          {/* <Analytics/> */}
         </body>
       </AnimationProvider>
     </html>
-  )
+  );
 }
