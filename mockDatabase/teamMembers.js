906b4deaa6e824d3f2c10cf9c8aff0ae24ab6ade
Joey Eamigh <55670930+JoeyEamigh@users.noreply.github.com>
2025-06-12 21:58:40 -0400

    add completed MBA

--- mockDatabase/teamMembers.js
+++ mockDatabase/teamMembers.js
@@ -1,94 +1,115 @@
 const teamMembers = [
-    {
-        name: "Devon Gethers",
-        role: "Managing Partner",
-        imageSrc: "/headshotDevon.png",
-        title: "Managing Partner",
-        linkedin: "https://www.linkedin.com/in/devon-gethers/",
-        email: "devon@meridianventures.co",
-        medium: "https://medium.com/@devon_45585",
-        focus: "Devon allocates his time to identifying startups with high growth potential, raising capital to fund the opportunities, and investing time in the portfolio and its founders.",
-        education: "Devon holds a B.S. in Finance from the University of Utah, where he graduated Summa Cum Laude. He is currently a candidate at Harvard Business School, Class of 2025.",
-        experienceP1: "Prior to co-founding Meridian, Devon founded EarlyAdmit, an EdTech startup dedicated to improving educational and professional outcomes for underserved minorities, which he sold to private equity in 2023.",
-        experienceP2: "While scaling EarlyAdmit, Devon worked in a $2B AUM growth equity firm investing in Enterprise SaaS and Branded Consumer businesses. Devon's blend of operator success and investment expertise makes him a preferred strategic partner to founders."
-    },
-    {
-        name: "Karlton Haney",
-        role: "Managing Partner",
-        imageSrc: "/headshotKarlton.png",
-        title: "Managing Partner",
-        linkedin: "https://www.linkedin.com/in/kevinkarltonhaney/",
-        email: "karlton@meridianventures.co",
-        focus: "Karlton focuses his time on sourcing startups, making new investments, and managing relationships with portfolio companies.",
-        education: "Karlton holds a B.S. in Industrial Engineering from the University of Arkansas, where he graduated Summa Cum Laude. He is currently a candidate at Harvard Business School, Class of 2025.",
-        experienceP1: "Prior to co-founding Meridian, Karlton was a private equity buyout and growth investor at The Stephens Group. In his role at Stephens Group, he reviewed new investment opportunities, supported the team’s investment efforts by conducting financial analysis, due diligence, and industry research, and worked closely with portfolio companies on strategic priorities. Before Stephens Group, he worked in various finance and strategy roles at Walmart Inc."
-    },
-    {
-        name: "Dallin Anderson",
-        role: "Advisor",
-        imageSrc: "/headshotDallin.png",
-        title: "Advisor",
-        linkedin: "https://linkedin.com/in/dallinanderson",
-        focus: "Fund strategy and new investment advisory.",
-        education: "B.S. in Accounting from Brigham Young University. MBA from Harvard Business School.",
-        experienceP1: "Mr. Anderson was the Founder, Chairman, and CEO of Montigen Pharmaceuticals (acquired by Supergen) and Co-Founder, Chairman, and President of Tolero Pharmaceuticals (acquired by Dainippon Sumitomo).",
-        experienceP2: "Dallin founded and currently manages Serial Capital, an investor in real estate and seed/growth stage companies across various sectors."
-    },
-    {
-        name: "Ken Goldman",
-        role: "Advisor",
-        imageSrc: "/headshotKen.png",
-        title: "Advisor",
-        linkedin: "https://www.linkedin.com/in/ken-goldman-552a472/",
-        focus: "Fund strategy, network, and portfolio advisement.",
-        education: "B.S. in Electrical Engineering from Cornell University and an M.B.A. from Harvard Business School.",
-        experienceP1: "Mr. Goldman is the former President of Hillspire. Previously, he was the CFO of Yahoo!, Fortinet, and Siebel Systems, Inc. Over his 40-year career, Mr. Goldman has helped take three companies public, including VLSI, Excite@Home, and Fortinet.",
-        experienceP2: "Mr. Goldman has extensive board experience, serving on over 40 corporate boards, including Fortinet, RingCentral, Zuora, and GoPro, Inc. He also sits on private and non-profit boards such as Wealthfront and the RFK Human Rights Board."
-    },
-    {
-        name: "Curtis Feeny",
-        role: "Advisor",
-        imageSrc: "/headshotCurtis.png",
-        title: "Advisor",
-        linkedin: "https://www.linkedin.com/in/curtisfeeny/",
-        focus: "Fund strategy, network, portfolio advisement.",
-        education: "MBA from Harvard Business School and a B.S. in M.E. from Texas A&M University.",
-        experienceP1: "Curtis is a Senior Advisor at Peterson Partners. Prior to Peterson Partners, Curtis was a General Partner in venture capital funds at Voyager Capital and Silicon Valley Data Capital. Prior to Voyager Capital and Silicon Valley Data Capital, he was Executive Vice President of Stanford Management Company and a Partner at Trammell Crow Company.",
-        experienceP2: "Curtis has served on 35 public and private boards. He recently served on the boards of CBRE (NYSE) and Staples (NYSE) and is currently on the boards of Khan Academy and Stanford Federal Credit Union."
-    },
-    {
-        name: "Heather Harmon",
-        role: "Advisor",
-        imageSrc: "/headshotHeather.png",
-        title: "Advisor",
-        linkedin: "https://www.linkedin.com/in/heatheraharmon/",
-        focus: "New investment advisory and mentorship for portfolio founders.",
-        education: "Heather completed a Pre-medicine degree from UC San Diego and University of Washington, attended the Universidad of Guadalajara, and UC Berkeley Haas School of Business for Artificial Intelligence.",
-        experienceP1: "Ms. Harmon is a 4x founder and former venture-backed COO and public company executive. She is a passionate proptech and fintech leader who has built and managed highly engaged technical teams, led product management, and scaled operations from startup through M&A.",
-        experienceP2: "She is currently is advising early stage companies on raising capital, defining product roadmaps, and scaling operations."
-    },
-    {
-        name: "Blake Modersitzki",
-        role: "Advisor",
-        imageSrc: "/headshotBlake.png",
-        title: "Advisor",
-        linkedin: "https://www.linkedin.com/in/blakemodersitzki/",
-        focus: "Fund strategy, sourcing, and portfolio advisement.",
-        education: "B.S. in Economics from Brigham Young University.",
-        experienceP1: "Blake Modersitzki is the Managing Partner at Pelion Venture Partners, where he focuses on early-stage technology investments. Blake has raised over $1B AUM across numerous funds.",
-        experienceP2: "With over two decades at Pelion, Blake is known for investing in the next frontier of technology. Blake invested in Red Hat, Fusion-io, Metacloud, Domo, Weave, MX, and more."
-    },
-    {
-        name: "Mark Fernandes",
-        role: "Advisor",
-        imageSrc: "/headshotMark.png",
-        title: "Advisor",
-        linkedin: "https://www.linkedin.com/in/markjfernandes/",
-        focus: "New deal advisory and fund strategy.",
-        education: "B.S. from Mangalore University, M.S. from the University of California, Berkeley, and an M.B.A. from Harvard Business School.",
-        experienceP1: "Mark Fernandes is a Managing Partner at Sierra Ventures and has extensive experience as a successful software investor. He currently serves on the boards of Reify Health, Yalo, Spectro Cloud, and other private companies.",
-        experienceP2: "His portfolio includes exits such as Sourcefire (IPO, acquired by Cisco for $2.7B), RedLock (acquired by Palo Alto Networks), Ooyala (acquired by Telstra), Frontbridge (acquired by Microsoft), Opalis (acquired by Microsoft) and DynamicOps (acquired by VMware)."
-    }
-  ];
+  {
+    name: 'Devon Gethers',
+    role: 'Managing Partner',
+    imageSrc: '/headshotDevon.png',
+    title: 'Managing Partner',
+    linkedin: 'https://www.linkedin.com/in/devon-gethers/',
+    email: 'devon@meridianventures.co',
+    medium: 'https://medium.com/@devon_45585',
+    focus:
+      'Devon allocates his time to identifying startups with high growth potential, raising capital to fund the opportunities, and investing time in the portfolio and its founders.',
+    education:
+      'Devon holds a B.S. in Finance from the University of Utah, where he graduated Summa Cum Laude. He was awarded an MBA from Harvard Business School, Class of 2025.',
+    experienceP1:
+      'Prior to co-founding Meridian, Devon founded EarlyAdmit, an EdTech startup dedicated to improving educational and professional outcomes for underserved minorities, which he sold to private equity in 2023.',
+    experienceP2:
+      "While scaling EarlyAdmit, Devon worked in a $2B AUM growth equity firm investing in Enterprise SaaS and Branded Consumer businesses. Devon's blend of operator success and investment expertise makes him a preferred strategic partner to founders.",
+  },
+  {
+    name: 'Karlton Haney',
+    role: 'Managing Partner',
+    imageSrc: '/headshotKarlton.png',
+    title: 'Managing Partner',
+    linkedin: 'https://www.linkedin.com/in/kevinkarltonhaney/',
+    email: 'karlton@meridianventures.co',
+    focus:
+      'Karlton focuses his time on sourcing startups, making new investments, and managing relationships with portfolio companies.',
+    education:
+      'Karlton holds a B.S. in Industrial Engineering from the University of Arkansas, where he graduated Summa Cum Laude. He was awarded an MBA from Harvard Business School, Class of 2025.',
+    experienceP1:
+      'Prior to co-founding Meridian, Karlton was a private equity buyout and growth investor at The Stephens Group. In his role at Stephens Group, he reviewed new investment opportunities, supported the team’s investment efforts by conducting financial analysis, due diligence, and industry research, and worked closely with portfolio companies on strategic priorities. Before Stephens Group, he worked in various finance and strategy roles at Walmart Inc.',
+  },
+  {
+    name: 'Dallin Anderson',
+    role: 'Advisor',
+    imageSrc: '/headshotDallin.png',
+    title: 'Advisor',
+    linkedin: 'https://linkedin.com/in/dallinanderson',
+    focus: 'Fund strategy and new investment advisory.',
+    education: 'B.S. in Accounting from Brigham Young University. MBA from Harvard Business School.',
+    experienceP1:
+      'Mr. Anderson was the Founder, Chairman, and CEO of Montigen Pharmaceuticals (acquired by Supergen) and Co-Founder, Chairman, and President of Tolero Pharmaceuticals (acquired by Dainippon Sumitomo).',
+    experienceP2:
+      'Dallin founded and currently manages Serial Capital, an investor in real estate and seed/growth stage companies across various sectors.',
+  },
+  {
+    name: 'Ken Goldman',
+    role: 'Advisor',
+    imageSrc: '/headshotKen.png',
+    title: 'Advisor',
+    linkedin: 'https://www.linkedin.com/in/ken-goldman-552a472/',
+    focus: 'Fund strategy, network, and portfolio advisement.',
+    education: 'B.S. in Electrical Engineering from Cornell University and an M.B.A. from Harvard Business School.',
+    experienceP1:
+      'Mr. Goldman is the former President of Hillspire. Previously, he was the CFO of Yahoo!, Fortinet, and Siebel Systems, Inc. Over his 40-year career, Mr. Goldman has helped take three companies public, including VLSI, Excite@Home, and Fortinet.',
+    experienceP2:
+      'Mr. Goldman has extensive board experience, serving on over 40 corporate boards, including Fortinet, RingCentral, Zuora, and GoPro, Inc. He also sits on private and non-profit boards such as Wealthfront and the RFK Human Rights Board.',
+  },
+  {
+    name: 'Curtis Feeny',
+    role: 'Advisor',
+    imageSrc: '/headshotCurtis.png',
+    title: 'Advisor',
+    linkedin: 'https://www.linkedin.com/in/curtisfeeny/',
+    focus: 'Fund strategy, network, portfolio advisement.',
+    education: 'MBA from Harvard Business School and a B.S. in M.E. from Texas A&M University.',
+    experienceP1:
+      'Curtis is a Senior Advisor at Peterson Partners. Prior to Peterson Partners, Curtis was a General Partner in venture capital funds at Voyager Capital and Silicon Valley Data Capital. Prior to Voyager Capital and Silicon Valley Data Capital, he was Executive Vice President of Stanford Management Company and a Partner at Trammell Crow Company.',
+    experienceP2:
+      'Curtis has served on 35 public and private boards. He recently served on the boards of CBRE (NYSE) and Staples (NYSE) and is currently on the boards of Khan Academy and Stanford Federal Credit Union.',
+  },
+  {
+    name: 'Heather Harmon',
+    role: 'Advisor',
+    imageSrc: '/headshotHeather.png',
+    title: 'Advisor',
+    linkedin: 'https://www.linkedin.com/in/heatheraharmon/',
+    focus: 'New investment advisory and mentorship for portfolio founders.',
+    education:
+      'Heather completed a Pre-medicine degree from UC San Diego and University of Washington, attended the Universidad of Guadalajara, and UC Berkeley Haas School of Business for Artificial Intelligence.',
+    experienceP1:
+      'Ms. Harmon is a 4x founder and former venture-backed COO and public company executive. She is a passionate proptech and fintech leader who has built and managed highly engaged technical teams, led product management, and scaled operations from startup through M&A.',
+    experienceP2:
+      'She is currently is advising early stage companies on raising capital, defining product roadmaps, and scaling operations.',
+  },
+  {
+    name: 'Blake Modersitzki',
+    role: 'Advisor',
+    imageSrc: '/headshotBlake.png',
+    title: 'Advisor',
+    linkedin: 'https://www.linkedin.com/in/blakemodersitzki/',
+    focus: 'Fund strategy, sourcing, and portfolio advisement.',
+    education: 'B.S. in Economics from Brigham Young University.',
+    experienceP1:
+      'Blake Modersitzki is the Managing Partner at Pelion Venture Partners, where he focuses on early-stage technology investments. Blake has raised over $1B AUM across numerous funds.',
+    experienceP2:
+      'With over two decades at Pelion, Blake is known for investing in the next frontier of technology. Blake invested in Red Hat, Fusion-io, Metacloud, Domo, Weave, MX, and more.',
+  },
+  {
+    name: 'Mark Fernandes',
+    role: 'Advisor',
+    imageSrc: '/headshotMark.png',
+    title: 'Advisor',
+    linkedin: 'https://www.linkedin.com/in/markjfernandes/',
+    focus: 'New deal advisory and fund strategy.',
+    education:
+      'B.S. from Mangalore University, M.S. from the University of California, Berkeley, and an M.B.A. from Harvard Business School.',
+    experienceP1:
+      'Mark Fernandes is a Managing Partner at Sierra Ventures and has extensive experience as a successful software investor. He currently serves on the boards of Reify Health, Yalo, Spectro Cloud, and other private companies.',
+    experienceP2:
+      'His portfolio includes exits such as Sourcefire (IPO, acquired by Cisco for $2.7B), RedLock (acquired by Palo Alto Networks), Ooyala (acquired by Telstra), Frontbridge (acquired by Microsoft), Opalis (acquired by Microsoft) and DynamicOps (acquired by VMware).',
+  },
+];
 
-  export default teamMembers;
+export default teamMembers;
